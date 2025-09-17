e64e40d242abe38d17511f10407d703aada5b687
mvm <mauriciomenendez27@gmail.com>
2025-09-18 00:26:28 +0200

    Nuevos cambios agregados

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+// js/menu.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const sidebar = document.getElementById("sidebar");
+
+  menuToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+  });
+});
